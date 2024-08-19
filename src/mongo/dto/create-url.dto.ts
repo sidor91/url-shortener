f1ab5f8cb270356496fff34b25b1a158e9bb348e
sidor91 <sidorchik91@gmail.com>
2024-08-19 21:27:16 +0200

    Create create-url.dto.ts

---
+++ src/mongo/dto/create-url.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateUrlDto {
+  @IsString()
+  short_url: string;
+
+  @IsString()
+  long_url: string;
+
+  @IsNumber()
+  click_count: number;
+}
