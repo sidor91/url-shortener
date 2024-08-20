546b127a678b485aae9a6efed1358e31a1ef3d1d
sidor91 <sidorchik91@gmail.com>
2024-08-20 21:17:25 +0200

    ADD | rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,51 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisService } from './redis/redis.service';
 import { MongoModule } from './mongo/mongo.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RedisModule } from './redis/redis.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { UrlModule } from './url/url.module';
 
 import { RedisConfig } from './@configs/redis.config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_DB_URL),
     CacheModule.register(RedisConfig),
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [
+          {
+            limit: configService.get('THROTTLER_LIMIT'),
+            ttl: configService.get('THROTTLER_TTL_SECONDS'),
+          },
+        ],
+        storage: new ThrottlerStorageRedisService(),
+      }),
+    }),
     MongoModule,
     RedisModule,
     UrlModule,
   ],
   controllers: [AppController],
-  providers: [AppService, RedisService],
+  providers: [
+    AppService,
+    RedisService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
+
+
+     
