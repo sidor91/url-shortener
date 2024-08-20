a6ef41f349949f92b905660a063b7ee1e7e9a61b
sidor91 <sidorchik91@gmail.com>
2024-08-20 18:22:39 +0200

    ADD | mongo service/module

--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Url } from './schemas/Url.schema';
 import { Model } from 'mongoose';
-import { CreateUrlDto } from './dto/create-url.dto';
+import { CreateUrlDto } from 'src/mongo/dto/create-url.dto';
 
 @Injectable()
 export class MongoService {
   constructor(@InjectModel(Url.name) private urlModel: Model<Url>) {}
 
   async create(dto: CreateUrlDto): Promise<Url> {
-    const createdUrl = new this.urlModel(dto);
-    return createdUrl.save();
+    const createdRecord = new this.urlModel(dto);
+    return createdRecord.save();
   }
 
-  async findAll(): Promise<Url[]> {
-    return this.urlModel.find().exec();
+  async findOne(short_url: string): Promise<Url> {
+    return this.urlModel.findOne({ short_url }).exec();
+  }
+
+  async incrementClickCount({
+    short_url,
+  }: {
+    short_url: string;
+  }): Promise<void> {
+    await this.urlModel
+      .updateOne({ short_url }, { $inc: { click_count: 1 } })
+      .exec();
   }
 }
